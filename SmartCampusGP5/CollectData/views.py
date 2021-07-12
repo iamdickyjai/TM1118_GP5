094cd8d6db51da9ac27ac9a786da293946b16f84
unknown <ic2140@IC-L-W502g27.hh.polyu.hk>
2021-07-12 17:15:23 +0800

    Update css

--- SmartCampusGP5/CollectData/views.py
+++ SmartCampusGP5/CollectData/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from . import iot_mqtt
 from .models import Data
+from django.db.models import Count
 
 # Create your views here.
 def index(request):
     data = Data.objects.all()
+    venue = []
+    for datum in data:
+        skip = False
+        for item in venue:
+            if datum.loc == item:
+                skip = True
+                break
+        if not skip:
+            venue.append(datum.loc)        
     context={
         'data': data,
+        'avail_venue': venue,
     }
     return render(request, "data.html", context = context)
