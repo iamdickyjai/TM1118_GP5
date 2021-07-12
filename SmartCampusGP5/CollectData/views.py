68cf780a8bac734c25357b2401ea756214e98110
unknown <ic2140@IC-L-W502g27.hh.polyu.hk>
2021-07-12 15:51:41 +0800

    Update some css

--- SmartCampusGP5/CollectData/views.py
+++ SmartCampusGP5/CollectData/views.py
@@ -4,4 +4,8 @@ from .models import Data
 
 # Create your views here.
 def index(request):
-    return render(request, "data.html")
+    data = Data.objects.all()
+    context={
+        'data': data,
+    }
+    return render(request, "data.html", context = context)
