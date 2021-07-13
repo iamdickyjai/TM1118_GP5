28d7869800d82fb9dab25bdcf12dfd8c77319a79
unknown <ic2140@IC-L-W502g27.hh.polyu.hk>
2021-07-13 14:36:31 +0800

    Integrate with venue and time

--- SmartCampusGP5/SmartCampusGP5/settings.py
+++ SmartCampusGP5/SmartCampusGP5/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'CollectData.apps.CollectdataConfig',
+    'dashboard',
+    "VenueEvent.apps.VenueeventConfig",
+    "TimeEventData.apps.TimeeventdataConfig",
 ]
 
 MIDDLEWARE = [
