3a2a2954143aa7ae2907a7fe43331ec860b5538c
Turkey <soyiufai@gmail.com>
2021-07-14 21:06:49 +0800

    Dashboard fixed, only fetch 100 latest record now

--- SmartCampusGP5/dashboard/forms.py
+++ SmartCampusGP5/dashboard/forms.py
@@ -2,23 +2,33 @@ from django import forms
 from django.forms import ModelForm
 from CollectData.models import Data
 from multiselectfield import MultiSelectFormField
+from django.db.models import Count
+
 
 class DashboardForm(forms.Form):
     data = Data.objects.all().order_by("loc")
+    data2 = (
+        Data.objects.values_list("loc")
+        .annotate(location=Count("loc"))
+        .order_by("location")
+    )
+    venue_no = data2.count()
     venue = []
     for datum in data:
         skip = False
         for item in venue:
             if datum.loc == item:
-                skip =True
+                skip = True
                 break
         if not skip:
             venue.append(datum.loc)
 
-    tup = (("all", "--all--"), )
-    #tup = ()
+    tup = (("all", "All"),)
+    # tup = ()
     for item in venue:
         tup = tup + ((item, item),)
-    
+
     VENUE_CHOICES = tup
-    venue = forms.MultipleChoiceField(choices=VENUE_CHOICES, widget=forms.CheckboxSelectMultiple())
+    venue = forms.MultipleChoiceField(
+        choices=VENUE_CHOICES, widget=forms.CheckboxSelectMultiple()
+    )
--- SmartCampusGP5/dashboard/views.py
+++ SmartCampusGP5/dashboard/views.py
@@ -5,36 +5,46 @@ from CollectData.models import Data
 from .forms import DashboardForm
 from django.utils import timezone
 
-venue = [""]
+venue = []
 
 # Create your views here.
 def index(request):
     global venue
+    form = DashboardForm()
 
     if request.method == "POST":
-        form = DashboardForm(request.POST)
         context = {
             "form": form,
         }
-        if form.is_valid():
-            form_venue = form.cleaned_data["venue"]
-            print(form_venue)
-            if form_venue != "null":
-                venue = form_venue
+        venue = []
+        for x in range(0, form.venue_no):
+            result_form_web = request.POST.get("id_venue_{}".format(x))
+            if result_form_web:
+                venue.append(result_form_web)
+            print(venue)
+        # if form.is_valid():
+        #     form_venue = form.cleaned_data["venue"]
+        #     if form_venue != "null":
+        #         venue = form_venue
     else:
-        form = DashboardForm()
         context = {
             "form": form,
         }
-        venue = [""]
+        venue = []
 
-    return render(request, 'dashboard/dashboard.html', context)
+    return render(request, "dashboard/dashboard.html", context)
+
 
 def temp_data(request):
-    one_h_ago = timezone.now()-timezone.timedelta(hours=1)
-    #events = Data.objects.filter(loc__in=venue).filter(date_created__gte=one_h_ago)
-    events = Data.objects.filter(date_created__gte=one_h_ago)
-    if "all" not in venue:
-        events = events.filter(loc__in=venue)
-    data = serializers.serialize('json', events) #Translating Django models into JSON formats
-    return JsonResponse(data, safe=False) #Returns a string that contains an array object
+    # one_h_ago = timezone.now() - timezone.timedelta(hours=72)
+    # events = Data.objects.filter(loc__in=venue).filter(date_created__gte=one_h_ago)
+    # events = Data.objects.filter(date_created__gte=one_h_ago)
+    events = Data.objects.all()[:100]
+    if "All" not in venue:
+        events = Data.objects.filter(loc__in=venue)[:100]
+    data = serializers.serialize(
+        "json", events
+    )  # Translating Django models into JSON formats
+    return JsonResponse(
+        data, safe=False
+    )  # Returns a string that contains an array object
