3bf57ff25423e19452e071403c71b4a025bb865d
unknown <ic2140@IC-L-W502g27.hh.polyu.hk>
2021-07-15 16:28:36 +0800

    ASDAS

--- SmartCampusGP5/SmartCampusGP5/settings.py
+++ SmartCampusGP5/SmartCampusGP5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1j7dsahm2tc9exc&da&@hj-)b(6e3@88o0x-rcbv!zn(&gzgwm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
