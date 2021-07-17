77e599b3a18f6294b5c51a6943167a40bb8ed1e9
unknown <ic2140@IC-L-W502g27.hh.polyu.hk>
2021-07-17 11:47:40 +0800

    Final

--- SmartCampusGP5/VenueEvent/views.py
+++ SmartCampusGP5/VenueEvent/views.py
@@ -12,20 +12,22 @@ def readExcel(request):
     
     df = pd.read_excel("Venue-Event.xlsx")
 
+    # Start from 1, because the first row in the excel file is useless
+    # Loop through each row of the excel
     for x in range(1, len(df)):
+        # Create a new record in Event Table
         events = Event()
+        
+        # Fill in the field of the new record
         events.venue = df.loc[x, df.columns[0]].upper()
-        # events.date =  str(datetime.datetime.strptime(
-        #     str(df.loc[x, df.columns[1]]), "%Y-%m-%d %H:%M:%S"
-        # ).date())
-        # events.time_start = df.loc[x, df.columns[2]]
-        # events.time_end = df.loc[x, df.columns[3]]
         events_date =  datetime.datetime.strptime(str(df.loc[x, df.columns[1]]), "%Y-%m-%d %H:%M:%S")
 
         events.time_start = datetime.datetime.combine(events_date, df.loc[x, df.columns[2]])
         events.time_end = datetime.datetime.combine(events_date, df.loc[x, df.columns[3]])
         events.event = df.loc[x, df.columns[4]]
         events.instructor = df.loc[x, df.columns[5]]
+
+        
         events.save()
 
     return render(request, "excel.html")
